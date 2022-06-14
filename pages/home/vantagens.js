b6eb22690c91b582176c9e28fddb381f7f134ec1
Gabriel Cardoso <gabrielcrs2016@gmail.com>
2022-06-14 16:07:47 -0300

    att

--- pages/home/vantagens.js
+++ pages/home/vantagens.js
@@ -1,5 +1,6 @@
 import css from './vantagens.module.css'
 import {Container, Row, Col} from "reactstrap"
+import {BsCheck2Circle} from "react-icons/bs"
 
 export default function vantagens(){
     return(
@@ -7,6 +8,32 @@ export default function vantagens(){
             <div className={css.bgbanner}>
                 <Container>
                     <div className={css.title}>Vantagens em <br/> utilizar <span style={{color: '#ffc700'}}>PESA Energia</span></div>
+                    <Row>
+                        <Col md={3}></Col>
+                        <Col md={6} className={css.bgcard}>
+                            <div className={css.text}>
+                                <BsCheck2Circle className={css.icon}/> Lorem ipsum dolor sit amet, consectetur adipiscing
+                            </div>
+                            <div className={css.text}>
+                                <BsCheck2Circle className={css.icon}/> Lorem ipsum dolor sit amet, consectetur adipiscing
+                            </div>
+                            <div className={css.text}>
+                                <BsCheck2Circle className={css.icon}/> Lorem ipsum dolor sit amet, consectetur adipiscing
+                            </div>
+                            <div className={css.text}>
+                                <BsCheck2Circle className={css.icon}/> Lorem ipsum dolor sit amet, consectetur adipiscing
+                            </div>
+                            <div className={css.text}>
+                                <BsCheck2Circle className={css.icon}/> Lorem ipsum dolor sit amet, consectetur adipiscing
+                            </div>
+                            <div className={css.text}>
+                                <BsCheck2Circle className={css.icon}/> Lorem ipsum dolor sit amet, consectetur adipiscing
+                            </div>
+                            <div><button className={css.button1}>Botão CTA Principal</button></div>
+                            <div><button className={css.button2}>CTA Secundário</button></div>
+                        </Col>
+                        <Col md={3}></Col>
+                    </Row>
                 </Container>
             </div>
         </div>
